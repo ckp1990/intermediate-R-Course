6721c6eeee898209fe68b97de074938b209d792a
chadnan <chadnanpandey0404@gmail.com>
2022-10-11 11:40:07 +0530

    @ VS code test

--- Analysis-of-count-data.R
+++ Analysis-of-count-data.R
@@ -6,6 +6,13 @@
 rm(list=ls())
 getwd() ##checking for write directory
 
-bicycle <- read.csv("./data/nyc-east-river-bicycle-counts.csv",header = T)
+bicycle <- read.csv( "./data/nyc-east-river-bicycle-counts.csv", header = T )
 head(bicycle)
 names(bicycle)
+range(bicycle$Total)
+library(ggplot2)
+
+ggplot(bicycle,aes(x=Total))+geom_histogram(bins=10)
+mod1 <- glm(bicycle$Total~bicycle$Low.Temp...F.  ,  family = "poisson")
+summary(mod1)
+
